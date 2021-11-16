644cedf1612d4bc465165cc9ee54d1312545bc70
Majiga <majigaa@yahoo.com>
2021-11-16 19:26:09 +0800

    Create visualisation1.py

---
+++ src/visualisation1.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.write("""
+# Test app using Streamlit
+This is **too easy** and easy is the best :)
+""")
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
+
+st.write("""
+## Random spots in Perth
+""")
+
+df = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [-31.95, 115.85], columns=['lat', 'lon'])
+st.map(df)
